7a5a261732d2fe28404b0b67319ca45ca46d47e1
Aleksander Kamiński <babs.seed@onet.pl>
2022-01-26 11:47:46 +0100

    added second python file

---
+++ finalny/tablenames.py
@@ -0,0 +1,11 @@
+class Planet:
+    def __init__(self, id, identif, planet_type, mass, radius, atmosphere, star, star_distance, alien_aggression_level):
+        self.id = id
+        self.identif = identif
+        self.planet_type = planet_type
+        self.mass = mass
+        self.radius = radius
+        self.atmosphere = atmosphere
+        self.star = star
+        self.star_distance = star_distance
+        self.alien_aggression_level = alien_aggression_level
